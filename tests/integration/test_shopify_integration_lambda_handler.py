5acfb1d6516d94006fdb301e1e57c8c5b613fcf6
enrique <enrique_ayala@ymail.com>
2023-12-29 19:35:38 -0600

    Update pydantic==2.5.2

--- tests/integration/test_shopify_integration_lambda_handler.py
+++ tests/integration/test_shopify_integration_lambda_handler.py
@@ -3,11 +3,11 @@ import os
 from unittest import TestCase
 from unittest.mock import patch, Mock
 
-from app import lambda_handler
+from src.orders.integration.app import lambda_handler
 from botocore.exceptions import ClientError
 
 
-class ShopifyOrderIntegrationFunction(TestCase):
+class TestShopifyOrderIntegrationFunction(TestCase):
     def setUp(self):
         self.event_path = "./integration/events/"
         self.context = Mock()
@@ -18,8 +18,8 @@ class ShopifyOrderIntegrationFunction(TestCase):
         with open(os.path.join(self.event_path, filename), 'r') as file:
             return json.load(file)
 
-    @patch('app.logger')
-    @patch('app.lambda_client')
+    @patch('src.orders.integration.app.logger')
+    @patch('src.orders.integration.app.lambda_client')
     def test_successful_invocation(self, mock_lambda_client, mock_logger):
 
         mock_lambda_client.invoke.return_value = {
@@ -31,7 +31,7 @@ class ShopifyOrderIntegrationFunction(TestCase):
         mock_lambda_client.invoke.assert_called_once()
         mock_logger.info.assert_called_with("Order created.")
 
-    @patch('app.lambda_client')
+    @patch('src.orders.integration.app.lambda_client')
     def test_client_error_on_invoke(self, mock_lambda_client):
         mock_lambda_client.invoke.side_effect = ClientError(
             {"Error": {"Code": "500", "Message": "Boto3 Error"}}, "Invoke")
@@ -39,7 +39,7 @@ class ShopifyOrderIntegrationFunction(TestCase):
         with self.assertRaises(ClientError):
             lambda_handler(self.event, self.context)
 
-    @patch('app.lambda_client')
+    @patch('src.orders.integration.app.lambda_client')
     def test_lambda_error_response(self, mock_lambda_client):
         mock_lambda_client.invoke.return_value = {
             'Payload': Mock(read=Mock(return_value=json.dumps({"statusCode": 400, "body": json.dumps({"message": "Bad request"})}).encode("utf-8")))
